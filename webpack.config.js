92ab4db971745db3a3e3a17a69ed88db63ddb456
Nicolas Deschamps <huggyc@Mac-001.lan>
2025-05-12 21:51:24 +0200

    Configure GitHub Pages deployment: Add workflow and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ export default (env, argv) => {
 		output: {
 			filename: 'bundle.js',
 			path: path.resolve(__dirname, 'dist'),
+			publicPath: isProduction ? '/King-War/' : '/',
 		},
 		module: {
 			rules: [
